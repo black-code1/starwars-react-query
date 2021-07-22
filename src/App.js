1a84c4d36a920c2f37094a063eb96490381b5ded
black-code1 <legrandmahamat1@gmail.com>
2021-07-22 07:47:07 +0100

    The useQuery Hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { useState } from 'react';
-import Navbar from './components/Navbar'
+import React, { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query'
+import Navbar from './components/Navbar';
 import Planets from './components/Planets';
 import People from './components/People';
 
-function App() {
+const queryClient = new QueryClient()
 
-  const [page, setPage] = useState('page');
+function App() {
+  let [page, setPage] = useState('planets');
+
   return (
-    <div className="App">
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
       <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className="content">
-        { page === 'planets' ? <Planets /> : <People /> }
+        <Navbar setPage={setPage} />
+        <div className="content">
+          { page === 'planets' ? <Planets /> : <People /> }
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
-const Navbar = ({setPage}) => {
+import React from 'react';
+
+const Navbar = ({ setPage }) => {
   return ( 
     <nav>
       <button onClick={() => setPage('planets')}>Planets</button>
-      <button onClick={() => setPage('people')}>People</button>
+      <button onClick={() => setPage('chars')}>People</button>
     </nav>
-   );
+  );
 }
  
 export default Navbar;
